8db5ecb06d4c601c71b4e5510219e2e743f3418b
Raven <rdev1163@outlook.com>
2017-03-13 22:07:10 -0700

    added contact items

--- Frontend/src/app/components/contacts.component.ts
+++ Frontend/src/app/components/contacts.component.ts
@@ -3,7 +3,41 @@ import {Component} from '@angular/core';
 @Component({
 	selector: 'contacts-component',
 	template: `
-	<h1>CONTACTS</h1>
+	<div class="row">
+		<div class="col-xs-6">
+			<input-component label="Name:"></input-component>
+			<input-component label="Phone:"></input-component>
+			<input-component label="Email:"></input-component>
+			<input-component label="Position:"></input-component>
+		</div>
+		<div class="col-xs-6">
+			<table class="table table-bordered table-striped table-hover">
+				<tr>
+					<th>Date</th>
+					<th>Title</th>
+				</tr>
+			</table>
+				<tr>
+					<td>3/13/2017</td>
+					<td>Title 1</td>
+				</tr>
+				<tr>
+					<td>3/13/2017</td>
+					<td>Title 1</td>
+				</tr>
+				<tr>
+					<td>3/13/2017</td>
+					<td>Title 1</td>
+				</tr>
+		</div>
+	</div>
+	<div class="row">
+	Notes
+		<div class="row">
+			<input-component label="Title"></input-component>
+		</div>
+		<text-area placeholder="Text"></text-area>
+	</div>
 `
 
 
--- Frontend/src/app/components/home.component.ts
+++ Frontend/src/app/components/home.component.ts
@@ -9,71 +9,66 @@ import {QuotesComponent} from '../components/quotes.component';
 	template: `
 	<div class="container">
 	 	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-			<ul>
-				<li>
-					<button (click)="sideMenuClick()" class="btn">Menu</button>
-				</li>
-				<li>
+			<ul class="col-xs-12">
+				<li class="col-xs-2" (click)="sideMenuClick()">Menu</li>
+				<li class="col-xs-6">
 					<input type="search" />
 				</li>
-				<li>
-					<button class="btn">Options</button>
-				</li>
+				<li class="col-xs-2">Options</li>
 			</ul>
 		</div>
-		<ul class="nav nav-tabs">
-			 <li ngClass="active:tab === CONTACTS">
-				 <a class="tab" [routerLink]="['/contacts']">
-					<tab-heading>Contacts</tab-heading>
-				</a>
-			 </li>
-			 <li ngClass="active:tab === QUOTES">
-				 <a [routerLink]="['/quotes']">
-					 <tab-heading>Quotes</tab-heading>
-				</a>
-			 </li>
-		</ul>
-		<div *ngIf="sideMenu" class="row">
-		
-		<div class="row">
-		Companies:
-				<select class="form-control">
-					<option>Company 1</option>
-					<option>Company 1</option>
-					<option>Company 1</option>
-				</select>
-		
-		</div> 
-			<input-component label="Name"></input-component>
-			<input-component label="Address"></input-component>
-			<input-component label="City"></input-component>
-			<input-component label="ZipCode"></input-component>
-			<input-component label="Phone"></input-component>
-		<div class="row">
-			Misc: <text-area></text-area>
-		</div>
-		<div class="row">
-			 <table class="table table-bordered table-hover">
-				<tr>
-					<th>Contact</th>
-				</tr>	
-			 	<tr>
-					<td>Contact 1</td> 	
-				</tr>
-				<tr>
-					<td>Contact 2</td> 	
-				</tr>	
-				<tr>
-					<td>Contact 3</td> 	
-				</tr>	
-			</table>
-		</div>
-		
-		</div>
-		
-		<div class="tab-content">
-			<contacts-component class="tab-pane" role="tabpanel" [class.active]="tab===CONTACTS"></contacts-component>
-			<quotes-component class="tab-pane" role="tabpanel" [class.active]="tab===QUOTES"></quotes-component>
+		<content-area>
+			<ul class="nav nav-tabs nav-pills">
+				 <li ngClass="active:tab === CONTACTS">
+					 <a class="tab" [routerLink]="['/contacts']">
+						<tab-heading>Contacts</tab-heading>
+					</a>
+				 </li>
+				 <li ngClass="active:tab=== QUOTES">
+					 <a [routerLink]="['/quotes']">
+						 <tab-heading>Quotes</tab-heading>
+					</a>
+				 </li>
+			</ul>
+			<div class="tab-content">
+				<contacts-component class="tab-pane" role="tabpanel" [class.active]="tab===CONTACTS"></contacts-component>
+				<quotes-component class="tab-pane" role="tabpanel" [class.active]="tab===QUOTES"></quotes-component>
+			</div>
+		</content-area>
+		<div [class.active]="sideMenu" class="side-menu">
+			<div class="row">
+			Companies:
+					<select class="form-control">
+						<option>Company 1</option>
+						<option>Company 1</option>
+						<option>Company 1</option>
+					</select>
+			
+			</div> 
+				<input-component label="Name"></input-component>
+				<input-component label="Address"></input-component>
+				<input-component label="City"></input-component>
+				<input-component label="ZipCode"></input-component>
+				<input-component label="Phone"></input-component>
+			<div class="row">
+				Misc: <text-area></text-area>
+			</div>
+			<div class="row">
+				 <table class="table table-bordered table-hover">
+					<tr>
+						<th>Contact</th>
+					</tr>	
+					<tr>
+						<td>Contact 1</td> 	
+					</tr>
+					<tr>
+						<td>Contact 2</td> 	
+					</tr>	
+					<tr>
+						<td>Contact 3</td> 	
+					</tr>	
+				</table>
+			</div>
 		</div>
 	</div>
 `
@@ -86,7 +81,7 @@ export class HomeComponent implements OnInit {
 		return 'contacts';
 	}
 	public tab: string;
-	public sideMenu: boolean = true;
+	public sideMenu: boolean = false;
 
 	constructor(private route: ActivatedRoute) {
 	}
