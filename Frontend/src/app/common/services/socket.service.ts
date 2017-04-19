d5dea902e0d606a9cb87f24f1698dd7d4e45ed17
Raven <rdev1163@outlook.com>
2017-04-19 10:04:53 -0700

    communication both ways works

--- Frontend/src/app/common/services/socket.service.ts
+++ Frontend/src/app/common/services/socket.service.ts
@@ -4,16 +4,28 @@ import {Observable} from 'rxjs';
 
 @Injectable()
 export class SocketService {
-	public io = IO.connect('1729', {transport: ['websocket'], path: '/user.get'});
-
+public io;
 	public responseSocket(pathVerb: string, body: {}): Promise<any> {
-		return new Promise((resolve, reject) => {
+
 			console.log('socket?', this.io);
-			this.io.emit(pathVerb, body);
-			this.io.on(pathVerb + '.response', response => {
-				resolve(response);
-			});
-		})
+		this.io = IO('localhost:1729', {transport: ['polling', 'websocket']});
+		this.io.connect('user.get');
+		return new Promise((resolve, reject) => {
+			resolve(this.io.on('user.response', (response: any) => {
+				console.log('15', response);
+			}));
+
+		});
+		console.log('handler', handler);
+		handler.emit('user.get', ['one', 'two'], response => {
+			console.log('ack?', response);
+		});
+			return new Promise((resolve, reject) => {
+				this.io.on(pathVerb + '.response', handler => {
+					this.io.emit(pathVerb, body);
+						resolve(handler);
+					});
+				})
 	}
 
 }
