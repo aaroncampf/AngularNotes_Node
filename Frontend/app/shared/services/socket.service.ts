046f0ebcc0dae92d921171fba55ea84a1e76e1f2
Raven <rdev1163@outlook.com>
2017-06-11 11:30:43 -0700

    wired up quotes

--- Frontend/app/shared/services/socket.service.ts
+++ Frontend/app/shared/services/socket.service.ts
@@ -4,11 +4,11 @@ import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class SocketService {
-	public io: IO = IO('localhost:1729', {transport: ['polling', 'websocket']});
+	// public io: IO = IO('localhost:1729', {transport: ['polling', 'websocket']});
+	public io: IO = IO('52.55.177.110:1729', {transport: ['polling', 'websocket']});
 
 	public responseSocket(pathVerb: string, payload?: any): Observable<any> {
 		this.io.emit(pathVerb, payload);
-		// this.io = IO('52.55.177.110:1729', {transport: ['polling', 'websocket']});
 		return Observable.create(observer => {
 			this.io.on(pathVerb + '.response', payload => {
 				observer.next(payload);
